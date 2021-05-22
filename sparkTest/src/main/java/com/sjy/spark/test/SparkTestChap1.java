41433aa0f604287eec3ae00519333fa9974f1118
shengjunyang <3240802257@qq.com>
2021-05-22 23:17:45 +0800

    init

--- sparkTest/src/main/java/com/sjy/spark/test/SparkTestChap1.java
+++ sparkTest/src/main/java/com/sjy/spark/test/SparkTestChap1.java
@@ -1,17 +1,42 @@
 package com.sjy.spark.test;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SparkTestChap1 {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf();
-        SparkSession session = new SparkSession.Builder()
-                .config(conf)
-                .appName("RunModifyiedFilm:yuanhuan02@maoyan.com")
-                .enableHiveSupport().getOrCreate();
-        SQLContext sqlContext = session.sqlContext();
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("JavaSparkPi")
+                .getOrCreate();
+
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+
+        int slices = (args.length == 1) ? Integer.parseInt(args[0]) : 2;
+        int n = 100000 * slices;
+        List<Integer> l = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            l.add(i);
+        }
+
+        JavaRDD<Integer> dataSet = jsc.parallelize(l, slices);
+
+        int count = dataSet.map(integer -> {
+            double x = Math.random() * 2 - 1;
+            double y = Math.random() * 2 - 1;
+            return (x * x + y * y <= 1) ? 1 : 0;
+        }).reduce((integer, integer2) -> integer + integer2);
+
+        System.out.println("Pi is roughly " + 4.0 * count / n);
+
+        spark.stop();
+
 
     }
 }
